17b7c415a1fa0d82107015351c393aa2a5a71480
han <glghan@sina.com>
2016-03-15 16:46:50 +0800

    update config.php by glg

--- onethink/User/config.php
+++ onethink/User/config.php
@@ -15,6 +15,6 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'H)Y)HOPU@$@_U(%JI_BKLG#%^&$%&YTADF{Pkfl[wfytugiwe-=gdf'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:79970531@qq.com@112.74.19.227:3306/aladdin'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:79970531@qq.com@112.74.19.227:3306/aladdin_supplier'); // 数据库连接，使用Model方式调用API必须配置此项
 // define('UC_DB_DSN', 'mysql://mtuser:mtuser@115.29.7.155:3306/aladdin'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 't_'); // 数据表前缀，使用Model方式调用API必须配置此项
